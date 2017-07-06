465009933e003361c1d0c726188e950078355dd5
Pahnki Kim <pahnki.kim@pkworks.io>
2017-07-06 14:59:45 +0900

    update header sub menu

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,22 @@
 $('.header-menu > li').on('mouseover', function() {
-    $(this).find('.header-sub-menu').show();
+    var subMenu = $(this).find('.header-sub-menu');
+    var subMenuItemWidth = subMenu.find('li').outerWidth();
+    var subMenuItemMaxCount = 5;
+    var subMenuItemCount = subMenu.find('li').length;
+
+    var width = subMenuItemWidth
+        * Math.min(subMenuItemCount, subMenuItemMaxCount);
+
+    if ($(this).offset().left + width > $(window).width()) {
+        var subMenuLeft = $(window).width() - ($(this).offset().left + width);
+        subMenu.css('left', subMenuLeft);
+    }
+    else {
+        subMenu.css('left', 0);
+    }
+
+    subMenu.width(width);
+    subMenu.show();
 });
 
 $('.header-menu > li').on('mouseout', function() {
