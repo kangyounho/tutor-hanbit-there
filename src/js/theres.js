3fa7b960ae39b277140d511ce90f6375858c1c24
Pahnki Kim <pahnki.kim@pkworks.io>
2017-07-11 12:48:22 +0900

    add ht-tab

--- src/js/theres.js
+++ src/js/theres.js
@@ -18,3 +18,33 @@ $('.ht-section-there').css('background-image',
     'url(' + model.background + ')');
 $('.ht-there-name').html(model.name);
 $('.ht-there-summary').html(model.summary);
+
+$('.ht-tab-btns > li').on('click', function() {
+    if ($(this).hasClass('active')) {
+        return;
+    }
+
+    var tabIndex = $(this).index();
+
+    var tabBtns = $(this).parent('.ht-tab-btns').find('li');
+    tabBtns.removeClass('active');
+    $(tabBtns[tabIndex]).addClass('active');
+
+    var tabContents = $(this).parents('.ht-tab').find('.ht-tab-contents > li');
+    tabContents.removeClass('active');
+    $(tabContents[tabIndex]).addClass('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
